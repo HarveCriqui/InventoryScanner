3126478b3d5a3d25160f47dd2395fb8488d2940d
H-Criqui <hec26@humboldt.edu>
2022-12-27 16:07:10 -0800

    Set up sqlite
    Set up application to work with sqlite

---
+++ Cpp Aplication/InventoryScanner/InventoryScanner/main.cpp
@@ -0,0 +1,18 @@
+/*
+Harve Criqui
+Inventory Scanner
+Date : 12/27/2022
+*/
+
+
+#include <iostream>
+#include <stdio.h>
+#include <sqlite3.h>
+
+using namespace std;
+
+int main() {
+
+
+	return 0;
+}
